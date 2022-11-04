1d88a1e9606d2e66a2e9db016943087984435edd
mathvalenza <mvalenza@uolinc.com>
2022-11-04 14:57:55 -0300

    updating examples

--- tests/unit/exercicio-exemplo-vue.spec.js
+++ tests/unit/exercicio-exemplo-vue.spec.js
@@ -1,44 +1,65 @@
 import { shallowMount } from "@vue/test-utils";
 import ExercicioExemplo from "@/exercicios/exercicio-exemplo.vue";
 
-describe("HelloWorld.vue", () => {
+describe("ExercicioExemplo.vue", () => {
   it("renderiza props.nome quando informada", () => {
     const wrapper = shallowMount(ExercicioExemplo, {
       propsData: { nome: "Matheus" },
     });
 
-    expect(wrapper.text()).toMatch("Matheus");
+    expect(wrapper.text()).toMatch(
+      "Olá, Matheus. Seja bem-vindo(a) ao nosso site!"
+    );
   });
 
   it("renderiza o valor padrão quando não há props.nome", () => {
     const wrapper = shallowMount(ExercicioExemplo, {
-      propsData: { nome: null },
+      propsData: {
+        nome: null,
+      },
     });
-    expect(wrapper.text()).toMatch("visitante");
+
+    expect(wrapper.text()).toMatch(
+      "Olá, visitante. Seja bem-vindo(a) ao nosso site!"
+    );
   });
 
   it("exibe a mensagem de despedida sem a nota quando o usuário clicar em sair", async () => {
-    const wrapper = shallowMount(ExercicioExemplo, {
-      propsData: { nome: "Fulano" },
-    });
+    const wrapper = shallowMount(ExercicioExemplo);
 
-    await wrapper.find("button").trigger("click");
+    const botao = wrapper.find("button");
+
+    await botao.trigger("click");
 
     expect(wrapper.text()).toMatch(
       "Volte sempre! Quando puder, nos deixe uma avaliação"
     );
   });
 
-  it("exibe a mensagem de despedida com a nota quando o usuário clicar em sair", async () => {
-    const wrapper = shallowMount(ExercicioExemplo, {
-      propsData: { nome: "Fulano" },
-    });
+  it("exibe a mensagem de despedida com a nota quando usuário em sair", async () => {
+    const wrapper = shallowMount(ExercicioExemplo);
 
     const textInput = wrapper.find('input[type="number"]');
-    await textInput.setValue("8");
 
-    await wrapper.find("button").trigger("click");
+    await textInput.setValue("9");
 
-    expect(wrapper.text()).toMatch("Volte sempre! Obrigado pela nota 8");
+    const botao = wrapper.find("button");
+
+    await botao.trigger("click");
+
+    expect(wrapper.text()).toMatch("Volte sempre! Obrigado pela nota 9");
   });
+
+  // it("exibe a mensagem de despedida com a nota quando o usuário clicar em sair", async () => {
+  //   const wrapper = shallowMount(ExercicioExemplo, {
+  //     propsData: { nome: "Fulano" },
+  //   });
+
+  //   const textInput = wrapper.find('input[type="number"]');
+  //   await textInput.setValue("8");
+
+  //   await wrapper.find("button").trigger("click");
+
+  //   expect(wrapper.text()).toMatch("Volte sempre! Obrigado pela nota 8");
+  // });
 });
